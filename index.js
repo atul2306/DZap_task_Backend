4868f4193cb6b42ff95becdde0601337a989023d
atul2306 <atulranjan2306@gmail.com>
2024-01-01 03:30:49 +0530

    bug fixing

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const cors = require('cors');
 const app = express();
 require("dotenv").config();
 
+// Enable Cross-Origin Resource Sharing (CORS)
 app.use(cors());
+
+// Parse incoming requests with JSON payloads
 app.use(express.json());
 
-
+// API routes
 app.use("/api", require("./routes/cryptoRoutes"));
+
+// Default route for root path
 app.use('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
+// Error handling middleware
 app.use((error, req, res, next) => {
     if (res.headerSent) {
       return next(error);
@@ -19,9 +25,10 @@ app.use((error, req, res, next) => {
     res.status(500).json({ message: error.message || "Something went wrong" });
   });
 
+// Set the port from environment variables or use 8000 as default
 const PORT = process.env.PORT || 8000;
 
-
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
--- routes/cryptoRoutes.js
+++ routes/cryptoRoutes.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const userController = require('../controllers/cryptoController');
 
+// Create an instance of Express Router
 
 const router = express.Router();
 
+/**
+ * Route to get a list of top cryptocurrencies based on market cap in USD.
+ * @route GET /api/cryptocurrencies
+ * @group Cryptocurrency - Operations related to cryptocurrencies
+ * @returns {Array} 200 - An array of cryptocurrency objects containing id and name
+ * @throws {Error} 500 - Internal Server Error
+ */
 
 router.get('/cryptocurrencies', userController.getCryptoCurrencies);
+
+/**
+ * Route to convert a specified amount from a source cryptocurrency to a target currency.
+ * @route POST /api/convert
+ * @group Conversion - Operations related to currency conversion
+ * @param {string} sourceCrypto.body.required - The source cryptocurrency ID
+ * @param {number} amount.body.required - The amount to convert (non-negative number)
+ * @param {string} targetCurrency.body.required - The target currency
+ * @returns {Object} 200 - An object containing the converted amount
+ * @throws {Error} 400 - Bad Request (Invalid input)
+ * @throws {Error} 500 - Internal Server Error
+ */
+
 router.post('/convert', userController.convertCurrencies);
+
+/**
+ * Route to get a list of supported vs_currencies from the CoinGecko API.
+ * @route GET /api/supportedcurrencies
+ * @group Currency - Operations related to supported currencies
+ * @returns {Array} 200 - An array of supported vs_currencies
+ * @throws {Error} 500 - Internal Server Error
+ */
+
 router.get('/supportedcurrencies', userController.getSupportedCurrencies);
 
+// Export the router for use in other parts of the application
 
 module.exports = router;
